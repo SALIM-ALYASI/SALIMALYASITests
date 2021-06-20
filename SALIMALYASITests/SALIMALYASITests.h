0499e3187a7c9c0eeb89b186d2ab5bb5d99dc7a1
الياسي والأمل <r.m.alyasi@gmail.com>
2021-06-20 14:25:01 +0400

    1

---
+++ SALIMALYASITests/SALIMALYASITests.h
@@ -0,0 +1,18 @@
+//
+//  SALIMALYASITests.h
+//  SALIMALYASITests
+//
+//  Created by الياسي on 20/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SALIMALYASITests.
+FOUNDATION_EXPORT double SALIMALYASITestsVersionNumber;
+
+//! Project version string for SALIMALYASITests.
+FOUNDATION_EXPORT const unsigned char SALIMALYASITestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SALIMALYASITests/PublicHeader.h>
+
+
