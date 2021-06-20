f1ec27315296145e0a6a9dfba19dca618b52d5fa
الياسي والأمل <r.m.alyasi@gmail.com>
2021-06-20 15:07:28 +0400

    1

---
+++ SALIMALYASITests/SALIMALYASITests/SALIMALYASI.swift
@@ -0,0 +1,14 @@
+//
+//  SALIMALYASI.swift
+//  SALIMALYASITests
+//
+//  Created by الياسي on 20/06/2021.
+//
+
+ import Foundation
+public class SALIM {
+    private init() {}
+    public static func SALIMALYASI() -> String {
+        return "سالم صح "
+    }
+}
